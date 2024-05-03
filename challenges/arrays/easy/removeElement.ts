34387e968d056c4c9fae94cc74ca532972e032f6
Mathias Ritter <huskmigffs@gmail.com>
2024-05-03 12:33:58 +0200

    checklaterwhenconfirmemail

--- challenges/arrays/easy/removeElement.ts
+++ challenges/arrays/easy/removeElement.ts
@@ -1,3 +1,10 @@
 function removeElement(nums: number[], val: number): number {
-    
+    for(let i = 0; i < nums.length; i++)
+    {
+        if(nums[i] = val)
+        {
+            nums.splice(nums[i], 1);
+        }
+    }
+    return nums.length;
 };
